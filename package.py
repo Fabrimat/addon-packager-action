1f3c23b7443e9886d1cada725eeac54e9b734c57
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2022-10-11 22:19:31 +0000

    Allow creating mcpack and zip

--- package.py
+++ package.py
@@ -3,7 +3,10 @@ import shutil
 rootdir = '.'
 
 for dirs in os.listdir(rootdir):
-    if(not os.path.isfile(dirs) and not dirs.startswith(".")):
+    if (not os.path.isfile(dirs) and not dirs.startswith(".")):
         shutil.make_archive(dirs, 'zip', dirs)
-        os.rename(dirs+".zip", dirs+".mcaddon")
+        if (dirs.startswith("ma-")):
+            os.rename(dirs+".zip", dirs[3:]+".mcaddon")
+        elif (dirs.startswith("mp-")):
+            os.rename(dirs+".zip", dirs[3:]+".mcpack")
         shutil.rmtree(dirs)
